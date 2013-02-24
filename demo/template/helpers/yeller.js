aa5e0c3fe1207a051fd5cfcd416214dfb2352d47
Limin Shen <limin.shen@hotmail.com>
2013-02-24 00:45:48 -0600

    handleabar -> Handlebar

--- demo/template/helpers/yeller.js
+++ demo/template/helpers/yeller.js
@@ -1,4 +1,4 @@
-define(['handlebars'], function ( Handlebars ){
+define(['Handlebars'], function ( Handlebars ){
   function yeller ( context, options ) {
     // Assume it's a string for simplicity.
     return context + "!!!!!!oneone!!one1";
